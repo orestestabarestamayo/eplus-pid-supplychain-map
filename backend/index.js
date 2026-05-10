b41099717e6d962a4f48a021e32ee466e68d89ee
Usuario <Usuario@Tecnico.mdftechnology.br>
2026-05-10 11:15:14 -0300

    Enable CORS for frontend Integration (Wix / React)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('E+ PID SupplyChain Map Backend Running');
